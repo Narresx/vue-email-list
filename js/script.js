1378551c7cc01c5c089896c38a2c4a3c5d4ac8b1
Narresx <92748159+Narresx@users.noreply.github.com>
2022-02-04 15:49:44 +0100

    add api Boolean

--- js/script.js
+++ js/script.js
@@ -4,16 +4,26 @@ console.log(' Vue ok', Vue);
 
 Vue.config.devtools = true;
 
+/* Usando Vue.js, generare 10 indirizzi email e stamparli in pagina all'interno di una lista.*/
+
 const root = new Vue({
 
     el: '#root',
 
     data: {
 
+        maxItems: 10, // variabile che indica il numero di oggetti massimi 
+
+        emails: [], // Inizializzo un array vuoto dove saranno contenute le mie email
     },
 
     methods: {
 
+        getRandomEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res => {
+                console.log(res);
+            });
+        }
     },
 
 
