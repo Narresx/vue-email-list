c75d133ea8c54825542c21ce2e1397a154d63662
Narresx <92748159+Narresx@users.noreply.github.com>
2022-02-04 15:22:12 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log('js ok');
+
+console.log(' Vue ok', Vue);
+
+Vue.config.devtools = true;
+
+const root = new Vue({
+
+    el: 'root',
+
+    data: {
+
+    },
+
+    methods: {
+
+    },
+
+
+});
